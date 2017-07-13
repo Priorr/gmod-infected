2ee4d11ac3dc646ea436cbe0ffa1ee841ac1dd45
Priorr <priorrrrr@gmail.com>
2017-07-14 04:46:13 +1000

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,60 +1,58 @@
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
+AddCSLuaFile( "sv_rounds.lua" )
+AddCSLuaFile( "sv_players.lua" )
 
-local ply = LocalPlayer
-local TEAM_INIT_INFECTED, TEAM_INFECTED, TEAM_SURVIVOR, TEAM_DEAD, TEAM_SPECTATOR = 1, 2, 3 ,4 ,5
+local TEAM_INIT_INFECTED, TEAM_INFECTED, TEAM_SURVIVOR, TEAM_DEAD, TEAM_SPECTATOR = 1, 2, 3, 4, 5
 
 function GM:PlayerInitialSpawn( ply )
-  
-  PlayerInitiialSetup( ply )
-
+	PlayerInititialSetup( ply )
 end 
 
 
 function GM:PlayerShouldTakeDamage( ply )
-  return true
+    return true
 end
 
 function GM:CanPlayerSuicide( ply )
-  if ply:Team() != ply:Team(3)
-    return true
-  else
+	if ply:Team() != ply:Team(3) then
+		return true
+	end
     return false
 end
   
+-- Use tables for this
+function GM:PlayerDeath( ply, wep, killer ) 
+  	local killersteam = killer:Team()
+  	local plyteam = ply:Team() -- Call it once, not 50+ times
   
-function GM:PlayerDeath( ply, wep, killer )
-	
-  if ply:Team() == ply:Team(3) then
-    if killer == ply:Team(1) or ply:Team(2) then
-      ply:SetTeam(2)
-    elseif killer != ply:Team(1) or ply:Team(2) then
-      ply:SetTeam(5)
-    end
-      
-    if killer:Team() == ply:Team(1) then
-    	ply:Team() == ply:Team(2)
-      killer:Team() == ply:Team(2)
+    if plyteam == TEAM_SURVIVOR then
+        if killersteam == TEAM_INIT_INFECTED or plyteam == TEAM_INFECTED then
+            ply:SetTeam(TEAM_INFECTED)
+            return
+        elseif killersteam != TEAM_INIT_INFECTED or plyteam == TEAM_INFECTED then
+            ply:SetTeam(TEAM_SPECTATOR)
+            return
+        end
+
+        if killersteam == TEAM_INIT_INFECTED then
+            ply:SetTeam(TEAM_SPECTATOR)
+            killer:SetTeam(TEAM_INFECTED)
+            return
+        end
+    
+    -- stops running the ENTIRE CODE
+        if plyteam == TEAM_INFECTED then
+            if killersteam == TEAM_SURVIVOR then 
+                local ammobox = ents.Create("item_ammobox")
+                ammobox:SetPos( Vector( 0, 0, 10 ) )
+                ammobox:Spawn()
+            end
+        end 
     end
     
-    if ply:Team() == ply:Team(2) then
-    	if killer == ply:Team(3) then
-        local ammobox = ents.Create("item_ammobox")
-				ammobox:SetPos( Vector( 0, 0, 10 ) )
-				ammobox:Spawn()
-      else
-      end
-      	
-    end 
-   
     killerteam = team.GetName( killer( 1 ):Team() ) -- THIS MAY NOT WORK, IF THIS DOESNT COME IN CHAT THEN THIS IS PROBABLY THE ERROR // SAM
     if killerteam == TEAM_INFECTED then
-      chat.AddText(victim, " has been claimed by ", inflictor, " and is now part of the infected team.")
+        --chat.AddText(victim, " has been claimed by ", inflictor, " and is now part of the infected team.") -- sent to client, not run serverside
     end
-end
- 
-function GM:PlayerDisconnected(ply)
-      
-end
-      
 end
