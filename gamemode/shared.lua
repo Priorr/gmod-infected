4e3e7f84523c276f542119af8576c815445fbccf
Priorr <priorrrrr@gmail.com>
2017-07-14 04:48:35 +1000

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,11 +1,11 @@
 GM.Name = "Infected"
 GM.Author = "Meeno, Sam, Cain & Prior"
 
-DeriveGamemode("sandbox")
-DEFINE_BASECLASS("gamemode_sandbox")
+DeriveGamemode("base")
+DEFINE_BASECLASS("gamemode_base")
 
 team.SetUp( 1, "TEAM_INIT_INFECTED", Color(255,0,0) )
 team.SetUp( 2, "TEAM_INFECTED", Color (60,240,20) )
-team.Setup( 3, "TEAM_SURVIVOR", Color(0,0,255) )
-team.Setup( 4, "TEAM_DEAD", Color(80,80,80) )
-team.Setup( 5, "TEAM_SPECTATOR", Color(255,255,255) )
+team.SetUp( 3, "TEAM_SURVIVOR", Color(0,0,255) )
+team.SetUp( 4, "TEAM_DEAD", Color(80,80,80) )
+team.SetUp( 5, "TEAM_SPECTATOR", Color(255,255,255) )
