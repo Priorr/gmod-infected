afbb6d885e48e96a72ecfcb1e7245091327afbda
Priorr <priorrrrr@gmail.com>
2017-07-14 04:47:02 +1000

    Update round.lua

--- gamemode/round.lua
+++ gamemode/round.lua
@@ -1,40 +1,58 @@
-function RoundSetup()
-game.CleanUpMap() 
-timer.Create( "PreRound", GetConvar("infected_preround_time"):GetInit(), 1, RoundStart() )
-SpawnPlayers()
+Round = {}
+Round.State = nil -- use tables to store variables, less memory
+--ROUNDSTATE = ROUND_PREP
+  
+function RoundStart()
+	timer.Create( "RoundLength", GetConvar("infected_round_time"):GetInit(), 1, RoundEnd() )
+    local totalply = #player.GetAll()
+
+	for k, v in pairs(totalply) -- Put all players in a loop.
+    	local randnumber = math.random(0, totalply) -- Get a random player from everybody that is online.
+    	v[randnumber]:SetTeam(TEAM_INIT_INFECTED) -- Set that random player to the initial infected.
+    end
 end
 
-function RoundStart()
-timer.Create( "RoundLength", GetConvar("infected_round_time"):GetInit(), 1, RoundEnd() )
-playercount = player.GetCount()
-
-for k, v in pairs(player.GetAll())
-    math.random( 0, playercount ) = whoitgonbe
-    whoitgonbe:SetTeam(TEAM_INIT_INFECTED) -- This shit wont work, just leaving it like this till I figure it out.
-    end
+function RoundSetup()
+	game.CleanUpMap() 
+	timer.Create("PreRound", GetConvar("infected_preround_time"):GetInit(), 1, RoundStart())
+	SpawnPlayers()
 end
 
 function RoundActive()
+    hook.Add("PlayerDeath", "InfectedStats", function(vic, inf, killer )
+        if timer.Exists("RoundLength") then
+
+        end
+    end)
     
-  local players = player.GetAll()
+    local infected = {}
+    local survivors = {}
     
-	local infected = {}
-  local survivors = {}
-    
-  for k,v ipairs( players ) do
-  	if v:Alive() then
-    	if v:Team() == TEAM_SURVIVOR then
-        table.insert(survivors, v )
-      elseif v:Team() == TEAM_INFECTED then
-        table.insert(infected, v )
-      end
+    for k,v ipairs( players ) do
+        if v:Alive() then
+            if v:Team() == TEAM_SURVIVOR then
+                table.insert(survivors, v )
+            elseif v:Team() == TEAM_INFECTED then
+                table.insert(infected, v )
+            end
+        end
     end
-  end
   
-  if timer.Exists("PreRound") then
- 		if(#survivors == 0 and timer.TimerLeft <= 1) then
-      RoundEnd(
-	
+    if timer.Exists("RoundLength") then
+        if(#survivors == (0) then
+            RoundEnd(WIN_INFECTED)
+        elseif(#survivors <=1 and timer.TimerLeft("RoundLength") == 0) then -- dont pause you fucking cunt
+            RoundEnd(WIN_SURVIVOR)
+        end
+--define who wonned
+end
+      
+function RoundEnd(winningteam)
+    if table.sort( team.GetPlayers(TEAM_SURVIVOR) ) == 0 then
+        timer.Create( "PostRound", GetConvar("infected_postround_time"):GetInit(), 1, RoundStart() )
+        RoundWin(winningteam)
+    end  
+end
     	
       
 
