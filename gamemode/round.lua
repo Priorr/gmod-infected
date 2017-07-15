2c5747f4c9da65244d70789574519d14d692f3cb
Priorr <priorrrrr@gmail.com>
2017-07-15 14:49:41 +1000

    Update round.lua

--- gamemode/round.lua
+++ gamemode/round.lua
@@ -1,9 +1,12 @@
 Round = {}
 Round.State = nil -- use tables to store variables, less memory
 
-CreateConVar( "infected_preround_time", "60", 0, "Set the length of a pre-round in Infected." ) -- Creates a in-game command so that users can change the time, default 1 minute.
-CreateConVar( "infected_round_time", "720", 0, "Set the length of a round in Infected." ) -- Creates a in-game command so that users can change the time, default 12 minutes.
-CreateConVar( "infected_postround_time", "20", 0, "Set the legnth of the post-round in Infected." )-- Creates a in-game command so that users can change the time, default 1 minute.
+CreateConVar( "infected_preround_time", "60", 0, "Set the length of a pre-round in Infected." ) -- Creates a in-game command so that users can change the time, default 1 minute. ~Prior
+CreateConVar( "infected_round_time", "720", 0, "Set the length of a round in Infected." ) -- Creates a in-game command so that users can change the time, default 12 minutes.~Prior
+CreateConVar( "infected_postround_time", "30", "Set the legnth of the post-round in Infected." )-- Creates a in-game command so that users can change the time, default 1 minute.~Prior
+  
+WIN_SURVIVOR = TEAM_SURVIVOR
+WIN_INFECTED = TEAM_INFECTED
 
 function RoundStart()
 	timer.Create( "RoundLength", GetConVarNumber("infected_round_time"), 1, RoundEnd() ) -- Need to change GetConvarNumber too GetConvsr and ConVar:GetInt due to GetConVarNumber being deprecated
@@ -11,6 +14,7 @@ function RoundStart()
 	for k, v in pairs(totalply) do -- Put all players in a loop.
     	local randnumber = math.random(0, totalply) -- Get a random player from everybody that is online.
     	v[randnumber]:SetTeam(TEAM_INIT_INFECTED) -- Set that random player to the initial infected.
+    	-- add something to give the correct loadout for infect i think calling PlayerLoadout( ply ) will do it for everyone if it doesn't call it -meeno
     end
 end
 
@@ -40,10 +44,8 @@ function RoundActive()
     if timer.Exists("RoundLength") -- This returns a boolean, it wont work unless you add "== true" ~ Prior
         if(#survivors == (0) then
             RoundEnd(WIN_INFECTED)
-        	InfectedWin() -- Call a function so we can create pretty text and stuff, maybe effects/sounds showing that the infected won. E.g when Deaths win in deathrun.
         elseif(#survivors => 1 and timer.TimerLeft("RoundLength") == 0) then -- It shouldn't be less than one, it should be more then one?
             RoundEnd(WIN_SURVIVOR)
-        	SurvivorsWin() -- Call a function so we can create pretty text and stuff, maybe effects/sounds showing that the survivors won. E.g when Deaths win in deathrun.
         end
 end
       
@@ -51,4 +53,7 @@ function RoundEnd(winningteam)
     if table.sort( team.GetPlayers(TEAM_SURVIVOR) ) == 0 then
         timer.Create( "PostRound", GetConVarNumber("infected_postround_time"), 1, RoundStart() ) -- Need to change GetConvarNumber too GetConvsr and ConVar:GetInt due to GetConVarNumber being deprecated
     end  
+      
+     
+    hook.Call("InfectedRoundWin", nil, winningteam)
 end
