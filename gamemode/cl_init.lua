b232dcbffadfc32e86c06952b9fa7bd2d6593498
Priorr <priorrrrr@gmail.com>
2017-07-15 14:54:51 +1000

    Update cl_init.lua

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,24 +1,6 @@
-util.AddNetworkString("DisconnectText", ply )
-util.AddNetworkString("InfectedText", ply )
-util.AddNetworkString("PlayerConnectText", ply )
-util.AddNetworkString("PlayerSpawnText", ply )
-
-function ChatDisconnectText()
-  	net.Recieve("PlayerDisconnectText")
-	chat.AddText( Color(255, 0, 0), ply:Nick(), Color(0, 255, 0), "has left the server.") -- When player leaves, print a message in chat. ~Prior
-end
-
-function PlayerConnectText()
-  	net.Recieve("PlayerConnectText")
-	chat.AddText( Color(0, 255, 0), ply:Nick(), Color(255, 0, 0), "has connected to the server.") -- When a player connects, print a message in chat. ~Prior
-end
-
-function PlayerSpawnText()
-  	net.Recieve("PlayerSpawnText")
-	chat.AddText( Color(0, 255, 0), ply:Nick(), Color(255, 0, 0), "has joined the server.") -- When player joins, print a message in chat. ~Prior
-end
-
-function PlayerInfectedText()
-  	net.Recieve("PlayerInfectedText")
-  	chat.AddText(Color(0, 255, 0) victim:Nick(), Color(243, 255, 0), "has been infected by", Color(255, 0, 0), attacker:Nick(), Color(243, 255, 0), "and is now part of the infected team.")
+function Lang:Print( Prefix, varText )
+	if not varText then return end
+	if type( varText ) != "table" then varText = { varText } end
+	
+	chat.AddText( Color(255,255,255), "[", Prefixes[ Prefix ], Prefix, Color(255,255,255), "] ", unpack( varText ) )
 end
