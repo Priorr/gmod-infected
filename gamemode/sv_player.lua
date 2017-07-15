619c7af8eeab285f9a59c68416f2098c3ead2838
Priorr <priorrrrr@gmail.com>
2017-07-15 15:22:20 +1000

    Update sv_player.lua

--- gamemode/sv_player.lua
+++ gamemode/sv_player.lua
@@ -5,20 +5,20 @@ end
 
 SurvivorModels = 
 {
-  models/player/Group01/Male_01.mdl
-  models/player/Group01/Male_02.mdl,
-  models/player/Group01/Male_03.mdl,
-  models/player/Group01/Male_04.mdl,
-  models/player/Group01/Male_05.mdl,
-  models/player/Group01/Male_06.mdl,
-  models/player/Group01/Male_07.mdl,
-  models/player/Group01/Female_01.mdl,
-  models/player/Group01/Female_02.mdl,
-  models/player/Group01/Female_03.mdl,
-  models/player/Group01/Female_04.mdl,
-  models/player/Group01/Female_05.mdl,
-  models/player/Group01/Female_06.mdl,
-  models/player/Group01/Female_07.mdl
+  "models/player/Group01/Male_01.mdl",
+  "models/player/Group01/Male_02.mdl",
+  "models/player/Group01/Male_03.mdl",
+  "models/player/Group01/Male_04.mdl",
+  "models/player/Group01/Male_05.mdl",
+  "models/player/Group01/Male_06.mdl",
+  "models/player/Group01/Male_07.mdl",
+  "models/player/Group01/Female_01.mdl",
+  "models/player/Group01/Female_02.mdl",
+  "models/player/Group01/Female_03.mdl",
+  "models/player/Group01/Female_04.mdl",
+  "models/player/Group01/Female_05.mdl",
+  "models/player/Group01/Female_06.mdl",
+  "models/player/Group01/Female_07.mdl"
 }
 
 infectedweps = {weapon_knife, weapon_pistol}
