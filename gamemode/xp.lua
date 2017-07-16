6eb00c28c096aef843c5999015001ece6b2a9871
Priorr <priorrrrr@gmail.com>
2017-07-16 15:15:19 +1000

    Update xp.lua

--- gamemode/xp.lua
+++ gamemode/xp.lua
@@ -1,53 +1,4 @@
--- Levels 1-80
--- Prestige 1-20
-xp4levels = {
-800,
-1100,
-1200, 
-1800, 
-2200,
-2500,
-2800,
-3200,
-3600,
-4000,
-4400,
-4900,
-5400,
-5900,
-6400,
-6900,
-7400,
-7900,
-8400,
-9400,
-9900,
-10400,
-10900,
-11400,
-11900,
-12400,
-12900,
-13400,
-14000,
-14600,
-15200,
-15800,
-16400,
-17000,
-17600,
-18200,
-18800,
-19400,
-20000,
-20600,
-21200,
-21800,
-22400,
-23000,
-
-}    --can this not be done cleaner? 
-Filename = xpsystem
+Filename = xp.lua
 function ply:SavingStats()
     self:SetPData( "Level", self;GetNWInt( "Level" ) )
 	self:SetPData( "XP", self;GetNWInt( "XP" ) )
@@ -75,21 +26,22 @@ end
 
 function StatsAddXP( self, n )
         self:SetNWInt( "XP", self:GetNWInt( "XP" ) + n )
-        if( tonumber( self:GetNWInt( "XP" ) ) > 99 && tonumber ( self:GetNWInt( "Level" ) ) < 80 ) then
-          	local tempxp = self:GetNWInt( "XP" ) - 100
+        local xprequirement == tonumber( self:GetNWInt( "Level" ) * 300
+        if( tonumber( self:GetNWInt( "XP" ) ) > xprequirement - 1 && tonumber ( self:GetNWInt( "Level" ) ) < 80 ) then
+          	local tempxp = self:GetNWInt( "XP" ) - xprequirement
           	self:StatsLevelsUp()
             self:SetNWInt( "XP", tempxp )
           	print( "Level Up" )
-        elseif( tonumber( self:GetNWInt( "XP" ) ) > 99 && tonumber( self:GetNWInt( "Level" ) ) == 80 ) then
+        elseif( tonumber( self:GetNWInt( "XP" ) ) > xprequirement - 1 && tonumber( self:GetNWInt( "Level" ) ) == 80 ) then
           	local templvl = self:GetNWInt( "Level" ) - 80
-        	local tempxpp = self:GetNWInt( "XP" ) - 100
+        	local tempxpp = self:GetNWInt( "XP" ) - xprequirement
         	self:PrestigeLevelsUp()
         	self:SetNWInt( "Level", templvl )
         	self:SetNWInt( "XP", tempxpp )
         	print( "Prestige up" )
-        elseif( tonumber( self:GetNWInt( "XP" ) ) > 99 && tonumber( self:GetNWInt( "Level" ) ) == 80 && tonumber( self:GetNWInt( "Prestige" ) ) == 20 ) then
-        	self:SetNWInt( "XP", 100 )
-        	print( "WOW! You reached the max level. That is truly amazing." )
+        elseif( tonumber( self:GetNWInt( "XP" ) ) > xprequirement - 1 && tonumber( self:GetNWInt( "Level" ) ) == 80 && tonumber( self:GetNWInt( "Prestige" ) ) == 20 ) then
+        	self:SetNWInt( "XP", xprequirement )
+        	print( "WOW! You reached the max level. get a fuken life m8" )
         end
 end
 
@@ -102,10 +54,6 @@ function PrestigeLevelsUp()
 		self:SetPData( "XP", 0 )
         self:SetPData( "Level", 0 )
         self:SetNWInt( "Prestige", self:GetNWInt( "Prestige" ) + 1 )
-end
-      
-function xpscale( c, n )
-        return c * math.pow( n, 2.3 )
 end
 	
 hook.Add("PlayerInitialSpawn", "UpdatePlayerInfo", function(ply)
